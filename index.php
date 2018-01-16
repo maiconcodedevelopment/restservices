24a23f9c89ab85723d23cf74604f5fe06e046360
maiconcodedevelopment <codedevelopment@code.com>
2018-01-16 19:45:45 -0200

    login

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 header('Content-type','text/plain');
 
+define('env',TRUE) or die(Exception::class);
+
 $host = $_SERVER['HTTP_HOST'];
-$dir = dirname($_SERVER['PHP_SELF']);
+$dir = explode('/',dirname($_SERVER['PHP_SELF']))[1];
 $in = 'index.php';
 
-header("Location: http://$host$dir/$in");
+//header("Location: http://$host/$dir/$in");
+
+$_SERVER['HTTP_ACCEPT_CHARSET'] = 'uft-8';
